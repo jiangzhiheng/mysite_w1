45b9dac4dceef798765fc6a588ded09fef8f6a5a
蒋志恒 <1689991551@qq.com>
2018-08-12 12:07:41 +0800

    Initial commit

---
+++ app01/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+# Create your views here.
+
+
+# def login(request,nid):
+#     print(nid)
+#     return HttpResponse("Login")
+# def login2(request,p1,x1):
+#     print(p1,x1)
+#     return HttpResponse("Login2")
+USER_LIST = []
+for item in range(94):
+    temp = {'id':item,'username':'alex'+str(item),'email':'email'+str(item)}
+    USER_LIST.append(temp)
+
+
+
+def index(request, page):
+    print(page) #1.0-9  2.10-19 3.20-29
+    page = int(page)
+    start = (page-1)*10
+    end = page*10
+    user_list = USER_LIST[start:end]
+    return render(request,'index.html',{'user_list':user_list})
+
+
+def detail(request,nid):
+    nid = int(nid)
+    current_detail = USER_LIST[nid]     #current_detail列表中的一个元素，数据类型为字典
+    return render(request,'detail.html',{'current_detail':current_detail})
---
+++ mysite_w1/urls.py
@@ -0,0 +1,29 @@
+"""mysite_w1 URL Configuration
+# 路由系统
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import re_path
+from django.urls import path
+
+from app01 import views
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    # path('index/', views.index ),
+    # re_path('login/(\d+)/', views.login),
+    # re_path('login2/(?P<p1>\d+)/(?P<x1>\d+)/', views.login2),
+    re_path('index/(\d+)/', views.index),
+    re_path('detail/(\d+)/', views.detail),
+]
